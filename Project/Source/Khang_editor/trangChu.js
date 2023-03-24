ec93111ca34ddecf9e387fa994e2abf42ad6fe9c
khanghoai <khanglyhoai1@gmail.com>
2023-03-24 15:00:28 +0700

    trangChinh

---
+++ Project/Source/Khang_editor/trangChu.js
@@ -0,0 +1,14 @@
+var creNote_but = document.getElementsByClassName("icon");
+var creNote_view = document.getElementsByClassName("creNote_view");
+var exit = document.getElementsByClassName("exit");
+
+creNote_but[0].addEventListener("click",open_creNote_view);
+exit[0].addEventListener("click",exit_creNote);
+
+function open_creNote_view(){
+    creNote_view[0].style.display = "block";
+}
+
+function exit_creNote(){
+    creNote_view[0].style.display = "none";
+}
