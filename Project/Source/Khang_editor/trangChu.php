ed7f45488e39d7195fb0c45bc908e1a46c5796f4
khanghoai <khanglyhoai1@gmail.com>
2023-03-25 09:54:28 +0700

    chinhSua

---
+++ Project/Source/Khang_editor/trangChu.php
@@ -0,0 +1,18 @@
+<?php
+    $severname = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "notes";
+    $nameInput = $_POST['nameInput']; 
+
+    $conn = new mysqli($severname,$username,$password);
+
+    if(isset($nameInput)){
+        echo 'Have';
+    }
+    else{
+        $themNote = "INSERT INTO note (maNote,tenNote) VALUES ('001',$nameInput)";
+        mysqli_query($conn,$themNote);
+    }
+
+?>
---
+++ Project/Source/Khang_editor/trangChu_view.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel = "stylesheet" href="./trangChu.css">
+    <title>Trang Chu</title>
+</head>
+<body>
+    <?php
+        include 'trangChu.php';
+    ?>
+    <header></header>
+    <div class="main">
+        <div class="creNote_but">
+            <p class="icon">+</p>
+        </div>
+        <form action="./trangChu.php" class="creNote_view" method="post">
+            <h2 class="nameText">Tên note</h2>
+            <input type="text" class = "nameInput" name="nameInput">
+            <button type="sumbit" name="create" class="create"> Tạo </button>
+            <div class="creNote_exit">Exit</div>
+        </form> 
+    </div>
+</body>
+</html>
