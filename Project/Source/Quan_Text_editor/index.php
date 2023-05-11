b8c25ca5d180f4181b9f9bccd8ca278bd567e6d6
leequanno1 <binquan1234@gmail.com>
2023-05-11 19:08:24 +0700

    re update

--- Project/Source/Quan_Text_editor/index.php
+++ Project/Source/Quan_Text_editor/index.php
@@ -1,15 +1,10 @@
 <?php
     session_start();
     include "../connetdata.php";
-<<<<<<< HEAD
     $tennote = $_GET['tennote'];
     $tendn = $_SESSION['tendn'];
     $noidung =$tendn+"_"+$tennote+".txt";
     $file = "../NoteFile/"+$noidung;
-=======
-    $get_content = mysqli_query($conn,"SELECT tennote ,noidung FROM notes WHERE tennote = 'note 1'");
-    $row = mysqli_fetch_assoc($get_content);
->>>>>>> 42c743cdbcf65f9c07aabade95630de27fb425cb
 ?>
 
 <!DOCTYPE html>
@@ -25,7 +20,6 @@
   <link rel="stylesheet" href="style.css">
 </head>
 <body>
-<<<<<<< HEAD
   <div id="headerFinderDiv">
   </div>
   <div id="editorDiv">
@@ -42,21 +36,6 @@
       </textarea>
     </form>
   </div>
-=======
-  <form id="container">
-    <input type="text" name="" id="note_title" value="
-    <?php 
-        echo $row['tennote'];
-      ?>">
-    <button id="submitBtn">Lưu</button>
-
-    <textarea id="editor" class="editor">
-      <?php 
-        echo $row['noidung'];
-      ?>
-    </textarea>
-  </form>
->>>>>>> 42c743cdbcf65f9c07aabade95630de27fb425cb
   <script src="script.js"></script>
   <script src="eventLTN.js"></script>
 </body>
