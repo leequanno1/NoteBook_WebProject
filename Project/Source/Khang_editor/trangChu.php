561c60a187f4d93562798e360a8abe43c2049a92
khanghoai <khanglyhoai1@gmail.com>
2023-05-14 22:02:25 +0700

    update
    Cách này lỗi cần tìm cách khác

--- Project/Source/Khang_editor/trangChu.php
+++ Project/Source/Khang_editor/trangChu.php
@@ -4,15 +4,25 @@
     if ($_POST['tenNote']) {
         $tenNote = $_POST['tenNote'];
         $tendn = $_SESSION['tendn'];
-        $_SESSION['tennote'] = $tenNote;
-        $themNote = "INSERT INTO notes (tendn,tennote) VALUES ('$tendn','$tenNote')";
-        mysqli_query($conn,$themNote);
+        if(isset($_SESSION['tennote'])){
+            $_SESSION['tennote'] =  $_POST['name'];
+        }
+        else{
+            $_SESSION['tennote'] = $tenNote;
+            $themNote = "INSERT INTO notes (tendn,tennote) VALUES ('$tendn','$tenNote')";
+            mysqli_query($conn,$themNote);
+        }
         $noidung = $tendn.'_'.$tenNote.'.txt';
         $file = "../NoteFiles/".$noidung;
         $default = "<p>Viết tại đây</p>";
         $note = fopen($file, "w");
-        fwrite($note , $default);
+        fwrite($note,$default);
         fclose($note);
+        echo $_SESSION['tennote'];
+        echo "duoc";
         header("location: ../Quan_Text_editor/index.php");
+    }
+    else{
+        echo "Loi";
     }
 ?>
