a945560a1acf6c591c6414f5fe6e63afd60aa556
quynh <92347286+namisikirawa@users.noreply.github.com>
2023-05-17 22:10:47 +0700

    Update dynamicCalendar.js

--- Project/quynh_editor/dynamicCalendar.js
+++ Project/quynh_editor/dynamicCalendar.js
@@ -5,7 +5,7 @@ var thisMonth=['January','Febuary','March','April','May','June','July','August',
 var month=today.getMonth()
 var year=today.getFullYear()
 
-monthAndYear.innerText=thisMonth[month]+ " "+year
+monthAndYear.value=thisMonth[month]+ " "+year
 
 //next & prev icon
 var prevIcon=document.querySelector('.prevIcon')
@@ -31,7 +31,7 @@ for(i=1;i<=lastDateOfMonth;++i){
     else string+=`<li>${i}</li>`
 }
 
-//     //add inactive dates of next month
+   //add inactive dates of next month
 var firstDayOfNextMonth=new Date(year,month+1,1).getDay()
 var firstDateOfNextMonth=new Date(year,month+1,1).getDate()
 for(i=firstDayOfNextMonth;i<7;++i){
@@ -48,7 +48,7 @@ prevIcon.addEventListener('click',()=>{
         year-=1;
     }
     renderCalendar()
-    monthAndYear.innerText=thisMonth[month]+ " "+year
+    monthAndYear.value=thisMonth[month]+ " "+year
 })
 nextIcon.addEventListener('click',()=>{
     month=month+1
@@ -57,6 +57,51 @@ nextIcon.addEventListener('click',()=>{
         year+=1;
     }
     renderCalendar()
-    monthAndYear.innerText=thisMonth[month]+ " "+year
+    monthAndYear.value=thisMonth[month]+ " "+year
 })
+
+//change date and year input onchange
+var obj=document.querySelector('.monthAndYear')
+function warning(){
+    swal('Cảnh báo:','Thời gian không chính xác, vui lòng nhập lại!')
+}
+obj.addEventListener('change',(e)=>{
+    tmp=e.target.value
+    mNy=tmp.split(" ")
+    let m=mNy[0],y=mNy[1]
+    tmp=""
+    for(i=0;i<m.length;++i){
+        if(i==0) tmp+=m[i].toUpperCase()
+        else tmp+=m[i].toLowerCase()
+    }
+    m=tmp;
+    var check=false
+    // //neu onchange dung thi doi, con ko thi alert va tro lai nhu cu
+    for(i=0;i<thisMonth.length;++i){
+        if(m==thisMonth[i]){
+            month=i;
+            check=true;
+            break;
+        }
+    }
+    if(y>=1900 && y<=2050){
+        year=y;
+    } else check=false;
+    if(check){
+        monthAndYear.value=thisMonth[month]+' '+year
+        renderCalendar()
+    }else{
+        // alert('Tháng hoặc năm vừa nhập không chính xác, vui lòng nhập lại!')
+        warning() //sweet alert
+        monthAndYear.value=thisMonth[month]+' '+year
+        renderCalendar()
+    }
+})
+//chon ngay muon den bang thao tac click
+var x=document.querySelectorAll('.dateOfMonth li')
+for(i=0;i<x.length;++i){
+    x[i].addEventListener('click',(e)=>{
+        console.log(e.target.innerText)
+    })
+}
 
