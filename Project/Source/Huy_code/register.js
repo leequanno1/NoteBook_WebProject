0a50c3534b4731dc079e9303e5284310b74c5f2d
quynh <92347286+namisikirawa@users.noreply.github.com>
2023-05-30 20:49:12 +0700

    Update register.js

--- Project/Source/Huy_code/register.js
+++ Project/Source/Huy_code/register.js
@@ -26,12 +26,31 @@ btnRpElement.addEventListener('click', function() {
         currentType === 'password' ? 'text' : 'password'
     )
 })
-
+function checkEmail(s){
+    let atCount=0,dotCount=0
+    let atIndex,dotIndex
+    for(i=0;i<s.length;++i){
+        if(s.charAt(i)=='@'){
+            ++atCount;
+            atIndex=i;
+        }
+        else if(s.charAt(i)=='.'){
+            ++dotCount;
+            dotIndex=i;
+        }
+    }
+    if(atCount!=1 || dotCount!=1) return false
+    let x,y,z
+    x=s.substring(0,atIndex)
+    y=s.substring(atIndex+1,dotIndex)
+    z=s.substring(dotIndex+1)
+    return (x.length>=1 && y.length>=1&&z.length>=1)
+}
 tendn.addEventListener("change",()=>{
     text = tendn.value;
     if(text.length >= 8 && text.length <=20){
         tendn_ero[0].style.display = "none";
-        if(email.value.length != "" && pass.value.length >= 8 && pass.value.length <=20 && repass.value == pass.value){
+        if(checkEmail(email.value) && pass.value.length >= 8 && pass.value.length <=20 && repass.value == pass.value){
             btn_reg[0].removeAttribute('disabled');
         }
     }
@@ -39,25 +58,32 @@ tendn.addEventListener("change",()=>{
         tendn_ero[0].style.display = "block";
     }
 })
-
-email.addEventListener("change",()=>{
-    text = email.value;
-    if(text.length != ""){
-        email_ero[0].style.display = "none";
-        if(tendn.value.length >= 8 && tendn.value.length <=20 && pass.value.length >= 8 && pass.value.length <=20 && repass.value == pass.value){
-            btn_reg[0].removeAttribute('disabled');
-        }
+email.addEventListener('change',(e)=>{
+    if(checkEmail(e.target.value)){
+        email_ero[0].style.display='none'
     }
     else{
-        email_ero[0].style.display = "block";
+        email_ero[0].style.display='block'
     }
 })
+// email.addEventListener("change",()=>{
+//     text = email.value;
+//     if(text.length != ""){
+//         email_ero[0].style.display = "none";
+//         if(tendn.value.length >= 8 && tendn.value.length <=20 && pass.value.length >= 8 && pass.value.length <=20 && repass.value == pass.value){
+//             btn_reg[0].removeAttribute('disabled');
+//         }
+//     }
+//     else{
+//         email_ero[0].style.display = "block";
+//     }
+// })
 
 pass.addEventListener("change",()=>{
     text = pass.value;
     if(text.length >= 8 && text.length <=20){
         pass_ero[0].style.display = "none";
-        if(tendn.value.length >= 8 && tendn.value.length <=20 && email.value.length != "" && repass.value == pass.value){
+        if(tendn.value.length >= 8 && tendn.value.length <=20 && checkEmail(email.value) && repass.value == pass.value){
             btn_reg[0].removeAttribute('disabled');
         }
     }
@@ -70,7 +96,7 @@ repass.addEventListener("change",()=>{
     text = repass.value;
     if(text == pass.value){
         repass_ero[0].style.display = "none";
-        if(tendn.value.length >= 8 && tendn.value.length <=20 && email.value.length != "" && pass.value.length >= 8 && pass.value.length <=20){
+        if(tendn.value.length >= 8 && tendn.value.length <=20 && checkEmail(email.value) && pass.value.length >= 8 && pass.value.length <=20){
             btn_reg[0].removeAttribute('disabled');
         }
     }
